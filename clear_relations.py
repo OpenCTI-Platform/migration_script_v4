2e1c5b95fc010681b32af782a26fef7647310eb3
Samuel Hassine <samuel@hassine.fr>
2021-01-11 23:19:09 +0100

    Test

--- clear_relations.py
+++ clear_relations.py
@@ -10,7 +10,10 @@ print("[INITIALIZE] ElasticSearch connected")
 elasticsearch_client = Elasticsearch([elasticsearch_uri])
 
 print("[QUERY] ElasticSearch match " + id_to_clean)
-query = {"size": 10000, "query": {"match_phrase": {"connections.grakn_id.keyword": id_to_clean}}}
+query = {
+    "size": 10000,
+    "query": {"match_phrase": {"connections.grakn_id.keyword": id_to_clean}},
+}
 result = elasticsearch_client.search(index="stix_relations", body=query)
 # result = elasticsearch_client.delete(index="stix_relations", body=query)
 
